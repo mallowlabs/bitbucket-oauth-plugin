cdb757fb9e1eae2619b9c8ead4c42410fbcb1612
mallowlabs <mallowlabs@gmail.com>
2019-10-12 14:57:47 +0900

    repository moved

--- src/main/java/org/jenkinsci/plugins/api/BitbucketMissingPermissionException.java
+++
@@ -1,8 +0,0 @@
-package org.jenkinsci.plugins.api;
-
-public class BitbucketMissingPermissionException extends RuntimeException {
-
-    public BitbucketMissingPermissionException(String message) {
-        super(message);
-    }
-}
